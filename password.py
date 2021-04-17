57114b90ad7c54e32c77e4b8360ba0ee05191df7
Chris Tsai <g147573@gmail.com>
2021-04-17 22:13:44 +0800

    Modified password

--- password.py
+++ password.py
@@ -1,12 +1,15 @@
 i = 3
 while True: 
+	i = i - 1
 	password = input('請輸入密碼:')
 	if password == 'a123456':
 		print('登入成功!')
 		break
 	else:
-		i = i - 1
-		print('密碼錯誤!你還有',i,'次機會')
 		if i == 0:
+			print('帳號已鎖定!')
 			break
+		else:
+			print('密碼錯誤!你還有',i,'次機會')
+		
 
